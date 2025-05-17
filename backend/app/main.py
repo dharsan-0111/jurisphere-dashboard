30d489be611a24652deb08e41ecf2eed1c713426
dharsan-0111 <dharsan0100@gmail.com>
2025-05-17 15:13:46 +0530

    final cleanups in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,3 @@
-# app/main.py
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.data import customers
@@ -9,7 +7,7 @@ app = FastAPI()
 # Allow requests from frontend (localhost:3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust if needed
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
